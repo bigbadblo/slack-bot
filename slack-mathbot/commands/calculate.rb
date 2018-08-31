cbb6221f6e19491e03a74fb9b068456b870f7b06
Stephen Becker IV <github@deathbyescalator.com>
2018-08-31 10:17:25 -0700

    Update calculate.rb

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -5,13 +5,8 @@ module SlackMathbot
       command 'calculate'
 
       def self.call(client, data, match)
-        result = Dentaku::Calculator.new.evaluate(match[:expression]) if match.names.include?('expression')
-        result = result.to_s if result
-        if result && result.length > 0
-          client.say(channel: data.channel, text: result)
-        else
-          client.say(channel: data.channel, text: 'Got nothing.', gif: 'nothing')
-        end
+        `node index.js tvoff`        
+          client.say(channel: data.channel, text: 'tv off.', gif: 'nothing')
       rescue StandardError => e
         client.say(channel: data.channel, text: "Sorry, #{e.message}.", gif: 'idiot')
       end
