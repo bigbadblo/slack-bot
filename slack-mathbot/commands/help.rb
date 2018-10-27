bee908ddcffd90ad5366f7f81b1247b49a2f4884
bigbadblo <bigbadblo@tierthree.com>
2018-10-27 13:07:37 -0700

    Update help.rb

--- slack-mathbot/commands/help.rb
+++ slack-mathbot/commands/help.rb
@@ -2,7 +2,7 @@ module SlackMathbot
   module Commands
     class Help < SlackRubyBot::Commands::Base
       command 'help' do |client, data, _match|
-        client.say(channel: data.channel, text: 'See https://github.com/dblock/slack-mathbot, please.', gif: 'help')
+        client.say(channel: data.channel, text: 'See https://github.com/dblock/slack-mathbot, please.')
       end
     end
   end
